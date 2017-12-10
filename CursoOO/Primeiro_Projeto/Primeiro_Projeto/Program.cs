50594b6d5e22725e55b261c141b8ce27a380b288
Cristiano_Couto <cristianocouto89@hotmail.com>
2017-12-10 12:58:45 -0200

    Criando o primeiro projeto no VS, somente hello world

---
+++ CursoOO/Primeiro_Projeto/Primeiro_Projeto/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Primeiro_Projeto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Olá Mundo!");
+            Console.ReadLine();
+        }
+    }
+}
