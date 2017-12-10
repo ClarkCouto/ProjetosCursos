62378df3d7da760c05ce1d1ff72e0106e7376b7b
Cristiano_Couto <cristianocouto89@hotmail.com>
2017-12-10 12:46:58 -0200

    Projeto inicial para teste de instalação do VS Code

---
+++ CursoOO/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace CursoOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Bom Dia!");
+
+            Console.ReadLine();
+        }
+    }
+}
