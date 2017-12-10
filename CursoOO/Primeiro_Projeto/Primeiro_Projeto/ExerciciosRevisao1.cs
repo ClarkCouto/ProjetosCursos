1edf7e51e42e053909651961c37a5dc3e174d94a
Cristiano_Couto <cristianocouto89@hotmail.com>
2017-12-10 15:50:49 -0200

    Exercício 1 - Revisão 1 (Agricultor)

---
+++ CursoOO/Primeiro_Projeto/Primeiro_Projeto/ExerciciosRevisao1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Primeiro_Projeto
+{
+    public class ExerciciosRevisao1
+    {
+        public static void Exercicio1()
+        {
+            Console.WriteLine("\nExercício 1 - Revisão 1");
+            Console.WriteLine("\tUm agricultor precisa aplicar veneno em suas plantações. Para isso ele precisa escolher um frasco e\n" +
+                              "\tuma bomba para aplicação.O frasco de veneno possui um volume de veneno líquido medido em ml(mililitro).\n" +
+                              "\tJá a bomba de aplicação possui uma vazão * medida em ml / s(mililitro por segundo).O agricultor deseja\n" +
+                              "\tsaber: dado o volume inicial de um frasco de veneno(em ml), a vazão da bomba(em ml / s), e um tempo(em s),\n" +
+                              "\tqual volume de veneno restará no frasco após o uso da bomba durante este tempo?\n");
+
+            int volumeFrasco = 0 , vazaoBomba = 0, tempoUtilizacao = 0;
+
+            //Ler o valor de A
+            bool valido = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Digite o volume do frasco (em ml): ");
+                    volumeFrasco = int.Parse(Console.ReadLine());
+                    valido = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+            }
+            while (!valido);
+            valido = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Digite a vazão da bomba (em ml/s): ");
+                    vazaoBomba = int.Parse(Console.ReadLine());
+                    valido = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+            }
+            while (!valido);
+            valido = false;
+            do
+            {
+                try
+                {
+                    Console.Write("Digite o tempo de utilização (em s): ");
+                    tempoUtilizacao = int.Parse(Console.ReadLine());
+                    valido = true;
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Valor inválido!");
+                }
+            }
+            while (!valido);
+
+            int quantidadeUtilizada = vazaoBomba * tempoUtilizacao;
+            int quantidadeRestante = volumeFrasco - quantidadeUtilizada;
+
+            if (quantidadeRestante >= 0)
+                Console.WriteLine("Sobrou " + quantidadeRestante + "ml no frasco\n");
+            if (quantidadeRestante < 0)
+                Console.WriteLine("Erro! Quantidade utilizada maior que a quantidade disponível!\n");
+        }
+        public static void Exercicio2()
+        {
+            Console.WriteLine("Exercício 2 - Revisão 1");
+        }
+        public static void Exercicio3()
+        {
+            Console.WriteLine("Exercício 3 - Revisão 1");
+        }
+        public static void Exercicio4()
+        {
+            Console.WriteLine("Exercício 4 - Revisão 1");
+        }
+        public static void Exercicio5()
+        {
+            Console.WriteLine("Exercício 5 - Revisão 1");
+
+        }
+        public static void Exercicio6()
+        {
+            Console.WriteLine("Exercício 6 - Revisão 1");
+
+        }
+        public static void Exercicio7()
+        {
+            Console.WriteLine("Exercício 7 - Revisão 1");
+
+        }
+        public static void Exercicio8()
+        {
+            Console.WriteLine("Exercício 8 - Revisão 1");
+
+        }
+        public static void Exercicio9()
+        {
+            Console.WriteLine("Exercício 9 - Revisão 1");
+
+        }
+        public static void Exercicio10()
+        {
+            Console.WriteLine("Exercício 10 - Revisão 1");
+
+        }
+
+    }
+}
--- CursoOO/Primeiro_Projeto/Primeiro_Projeto/Menu.cs
+++ CursoOO/Primeiro_Projeto/Primeiro_Projeto/Menu.cs
@@ -15,9 +15,11 @@ namespace Primeiro_Projeto
                     "\n1 - Entrada de Dados" +
                     "\n2 - Fórmula de Báskara" +
                     "\n3 - Média de Idades" +
+                    "\n4 - Exercícios de Revisão 1" +
                     "\n0 - Sair");
                 try
                 {
+                    Console.Write("Digite a opção desejada: ");
                     opcao = int.Parse(Console.ReadLine());
                     switch (opcao)
                     {
@@ -33,7 +35,9 @@ namespace Primeiro_Projeto
                         case 3:
                             MediaIdades.Calcular();
                             break;
-
+                        case 4:
+                            MenuExerciciosRevisao1();
+                            break;
                     }
                 }
                 catch(Exception e)
@@ -42,8 +46,73 @@ namespace Primeiro_Projeto
                 }
             }
             while (!sair);
+        }
 
-            
+        public static void MenuExerciciosRevisao1()
+        {
+            bool sair = false;
+            int opcao;
+            do
+            {
+                Console.WriteLine("\nMenu - Exercícios de Revisão 1" +
+                    "\n 1 - Agricultor" +
+                    "\n 2 - " +
+                    "\n 3 - " +
+                    "\n 4 - " +
+                    "\n 5 - " +
+                    "\n 6 - " +
+                    "\n 7 - " +
+                    "\n 8 - " +
+                    "\n 9 - " +
+                    "\n10 - " +
+                    "\n0 - Voltar menu principal");
+                try
+                {
+                    Console.Write("Digite a opção desejada: ");
+                    opcao = int.Parse(Console.ReadLine());
+                    switch (opcao)
+                    {
+                        case 0:
+                            Main(new string[1]);
+                            break;
+                        case 1:
+                            ExerciciosRevisao1.Exercicio1();
+                            break;
+                        case 2:
+                            ExerciciosRevisao1.Exercicio2();
+                            break;
+                        case 3:
+                            ExerciciosRevisao1.Exercicio3();
+                            break;
+                        case 4:
+                            ExerciciosRevisao1.Exercicio4();
+                            break;
+                        case 5:
+                            ExerciciosRevisao1.Exercicio5();
+                            break;
+                        case 6:
+                            ExerciciosRevisao1.Exercicio6();
+                            break;
+                        case 7:
+                            ExerciciosRevisao1.Exercicio7();
+                            break;
+                        case 8:
+                            ExerciciosRevisao1.Exercicio8();
+                            break;
+                        case 9:
+                            ExerciciosRevisao1.Exercicio9();
+                            break;
+                        case 10:
+                            ExerciciosRevisao1.Exercicio10();
+                            break;
+                    }
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Opção Inválida!");
+                }
+            }
+            while (!sair);
         }
     }
 }
