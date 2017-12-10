b593b8e67e865fa01174ab6bcf39504baa0ec7dd
Cristiano_Couto <cristianocouto89@hotmail.com>
2017-12-10 13:19:55 -0200

    Imprimir double formantando casas decimais

--- CursoOO/Primeiro_Projeto/Primeiro_Projeto/Program.cs
+++ CursoOO/Primeiro_Projeto/Primeiro_Projeto/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Globalization;
 
 namespace Primeiro_Projeto
 {
@@ -8,7 +9,11 @@ namespace Primeiro_Projeto
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Olá Mundo!");
+            Double numero = 10.35784;
+            Console.WriteLine(numero.ToString("F2")); /*Imprime conforme a localização do computador (Brasil imprime ,)*/
+            Console.WriteLine(numero.ToString("F4"), CultureInfo.InvariantCulture); /* Imprime sempre usando . ao invés de , */
             Console.ReadLine();
+
         }
     }
 }
